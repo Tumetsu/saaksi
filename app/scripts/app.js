e5dedc957dc49fedf3e03c72ff25eb8abfda971c
Tuomas Salmi <salmi.tuomas@gmail.com>
2015-10-07 00:26:23 +0300

    Project setup

---
+++ app/scripts/app.js
@@ -0,0 +1,51 @@
+'use strict';
+//$(document).foundation();     //Problem with Karma tests.
+/**
+ * @ngdoc overview
+ * @name weatherdownloaderApp
+ * @description
+ * # weatherdownloaderApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('saaksiApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/state1");
+        //
+        // Now set up the states
+        $stateProvider
+            .state('state1', {
+                url: '/state1',
+                templateUrl: 'partials/state1.html'
+            })
+            .state('state1.list', {
+                url: 'list',
+                templateUrl: 'partials/State1.list.html',
+                controller: function($scope) {
+                    $scope.items = ["A", "List", "Of", "Items"];
+                }
+            })
+            .state('state2', {
+                url: "/state2",
+                templateUrl: "partials/state2.html"
+            })
+            .state('state2.list', {
+                url: "/list",
+                templateUrl: "partials/state2.list.html",
+                controller: function($scope) {
+                    $scope.things = ["A", "Set", "Of", "Things"];
+                }
+            });
+    });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherdownloaderApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherdownloaderApp
+ */
+angular.module('saaksiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
