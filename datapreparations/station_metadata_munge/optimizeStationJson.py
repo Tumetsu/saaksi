d208956be5ec3aa22ebe4a342609e0f581348609
Tuomas Salmi <salmi.tuomas@gmail.com>
2015-10-25 18:43:49 +0200

    Added some extra filtering of json optimization script in Python util. Removes most useless data from json file of weather stations to minimize the file size

---
+++ datapreparations/station_metadata_munge/optimizeStationJson.py
@@ -0,0 +1,40 @@
+import json
+import re
+
+def get_interval(interval_string):
+    results = re.findall('(?P<time>\d\d?)\sminute', interval_string)
+    return int(results[0])
+
+def remove_duplicates():
+    pass
+
+with open('stationDatasets.json', 'r') as datafile:
+    dataset = json.load(datafile)
+    result = {}
+
+    for d in dataset:
+        if d["groups"] == "Weather stations":
+            sets = {}
+            for s in d['datasets']:
+                #take only daily and realtime datasets
+                daily_or_realtime_set = re.findall('vuorokausiarvot|Säähavainnot', s["title"])
+                if len(daily_or_realtime_set) > 0:
+                    sets[s['title']] = {
+                        'begin': s['begin'],
+                        'end': s['end'],
+                        'interval': get_interval(s['interval'])
+                    }
+
+            #if no datasets, do not include in results
+            if len(sets) > 0:
+                result[d['name_fi'].strip()] = {
+                    'name_en': d['name_en'],
+                    'fmisid': d['fmisid'],
+                    'wmo': d['wmo'],
+                    'lon': d['lon'],
+                    'lat': d['lat'],
+                    'datasets': sets
+                }
+
+    with open("stations_metadata.json", "w", encoding="utf8") as o:
+        json.dump(result, o, indent=4,  ensure_ascii=False)
